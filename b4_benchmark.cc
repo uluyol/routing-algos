7116c0ae2fa55845fee039d01971f763a5a55e27
Muhammed Uluyol <uluyol0@gmail.com>
2020-12-21 15:29:33 -0500

    b4_benchmark: compare to optimized max-min fair implementation
    Looks like with 1000 FGs on the link, the B4 implementation is
    >850,000x slower for unweighted max-min fair allocation than the
    optimized algorithm.
    
    I was expecting it to be much slower, but wow is that a difference.
    
    TODO: figure out how speed up the B4 implementation.

--- b4_benchmark.cc
+++ b4_benchmark.cc
@@ -1,6 +1,7 @@
 #include <random>
 
 #include "absl/memory/memory.h"
+#include "alg/max_min_fairness.h"
 #include "b4.h"
 #include "benchmark/benchmark.h"
 #include "path_provider.h"
@@ -41,18 +42,25 @@ class BenchWaterfillPathProvider : public PathProvider {
   int num_fgs_;
 };
 
-BandwidthFunc MakeFunc(std::mt19937_64& rng, int n, int num_fgs) {
+BandwidthFunc MakeFunc(std::mt19937_64& rng, int n, bool weighted,
+                       int num_fgs) {
   BandwidthFunc fn;
   int64_t total_bps = 0;
   for (int i = 0; i < n; i++) {
     total_bps +=
         std::uniform_int_distribution<int>(0, 100'000'000 / num_fgs)(rng);
-    fn.Push(i + 1, total_bps);
+    double fair_share = i + 1;
+    if (!weighted) {
+      // Allocation should increase at the same rate for all FGs
+      fair_share = total_bps;
+    }
+    fn.Push(fair_share, total_bps);
   }
   return fn;
 }
 
-void BM_B4_Waterfill(benchmark::State& state) {
+void BenchB4SingleLink(benchmark::State& state, int num_func_steps,
+                       bool weighted) {
   // Create network
   std::vector<Link> links;
   links.reserve(state.range(0) + 1);
@@ -79,7 +87,7 @@ void BM_B4_Waterfill(benchmark::State& state) {
   PerFG<BandwidthFunc> funcs;
   for (NodeId id = 0; id < state.range(0); id++) {
     funcs[FG{id, NodeId(state.range(0))}] =
-        MakeFunc(rng, state.range(1), state.range(0));
+        MakeFunc(rng, num_func_steps, weighted, state.range(0));
   }
 
   for (auto _ : state) {
@@ -89,16 +97,45 @@ void BM_B4_Waterfill(benchmark::State& state) {
   }
 }
 
-BENCHMARK(BM_B4_Waterfill)
-    ->Args({16, 1})
-    ->Args({16, 4})
-    ->Args({16, 16})
-    ->Args({64, 1})
-    ->Args({64, 4})
-    ->Args({64, 16})
-    ->Args({256, 1})
-    ->Args({256, 4})
-    ->Args({256, 16});
+void BM_B4_SingleLink_BandwidthFunc_8(benchmark::State& state) {
+  BenchB4SingleLink(state, 8, true);
+}
+
+BENCHMARK(BM_B4_SingleLink_BandwidthFunc_8)
+    ->RangeMultiplier(10)
+    ->Range(10, 1000);
+
+void BM_B4_SingleLink_Unweighted(benchmark::State& state) {
+  BenchB4SingleLink(state, 1, false);
+}
+
+void BenchSingleLinkMaxMinFairnessProblem(
+    benchmark::State& state, SingleLinkMaxMinFairnessProblem* problem) {
+  std::vector<std::vector<int64_t>> demands(
+      4, std::vector<int64_t>(state.range(0) / 4, 0));
+  std::mt19937_64 rng(0);
+  for (size_t i = 0; i < demands.size(); i++) {
+    for (size_t j = 0; j < demands[i].size(); j++) {
+      demands[i][j] = std::uniform_int_distribution<int64_t>(
+          0, 100'000'000 / state.range(0))(rng);
+    }
+  }
+  const int64_t capacity = 1'000'000'000;
+  std::vector<std::vector<int64_t>> allocs;
+  for (auto _ : state) {
+    int64_t waterlevel = problem->ComputeWaterlevel(capacity, demands);
+    problem->SetAllocations(waterlevel, demands, &allocs);
+  }
+  benchmark::DoNotOptimize(allocs);
+}
+
+void BM_MMF_SingleLink_Unweighted(benchmark::State& state) {
+  SingleLinkMaxMinFairnessProblem problem({.enable_tiny_flow_opt = true});
+  BenchSingleLinkMaxMinFairnessProblem(state, &problem);
+}
+
+BENCHMARK(BM_B4_SingleLink_Unweighted)->RangeMultiplier(10)->Range(10, 1000);
+BENCHMARK(BM_MMF_SingleLink_Unweighted)->RangeMultiplier(10)->Range(10, 1000);
 
 }  // namespace
 }  // namespace routing_algos
